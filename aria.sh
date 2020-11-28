d1e7f3fc313f4e64aac522d07df8de79c3c01ad0
Chizuru <chizuru@kanojo.tk>
2020-11-28 21:37:50 +0530

    improved aria.sh

--- aria.sh
+++ aria.sh
@@ -1,7 +1,16 @@
 export MAX_DOWNLOAD_SPEED=0
-export MAX_CONCURRENT_DOWNLOADS=3
+tracker_list=$(curl -Ns https://trackerslist.com/all_aria2.txt)
+export MAX_CONCURRENT_DOWNLOADS=7
+
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 \
-  --max-connection-per-server=10 --rpc-max-request-size=1024M \
-  --seed-time=0.01 --min-split-size=10M --follow-torrent=mem --split=10 \
+   --max-connection-per-server=10 --rpc-max-request-size=1024M \
+   --bt-tracker="[$tracker_list]" --bt-max-peers=0 --seed-time=0.01 --min-split-size=10M \
+   --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS
+   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
+   --peer-id-prefix=-qB4220- --user-agent=qBittorrent/4.2.2 \
+   --disk-cache=64M --file-allocation=prealloc --continue=true \
+   --max-file-not-found=5 --max-tries=20 --auto-file-renaming=true \
+   --bt-enable-lpd=true --seed-time=0.01 --seed-ratio=1.0 \
+   --bt-force-encryption=true --bt-require-crypto=true --bt-min-crypto-level=arc4 \
+   --content-disposition-default-utf8=true --http-accept-gzip=true --reuse-uri=true

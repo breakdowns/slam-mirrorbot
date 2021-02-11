71e705ffe390e254ac7ca63a16ad310bd78a412c
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-02-11 19:21:54 +0700

    Update anime.py

--- bot/modules/anime.py
+++ bot/modules/anime.py
@@ -256,7 +256,7 @@ def weebhelp(update, context):
 • `/chr`*:* search character
 • `/mng`*:* search manga
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/87b7878ee9d6273af566f.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
 ANIME_HANDLER = CommandHandler("al", anime)

35e636cc23cd86685aa9f016b64b3bd5ba2d5395
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-02-14 20:43:34 +0700

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -109,7 +109,7 @@ class GoogleDriveHelper:
         msg = ''
         try:
             res = self.__service.files().delete(fileId=file_id, supportsTeamDrives=IS_TEAM_DRIVE).execute()
-            msg = "File/Folder Deleted Successfully"
+            msg = "Successfully deleted"
         except HttpError as err:
             LOGGER.error(str(err))
             if "File not found" in str(err):

13aea27a19806afbb7bd7f76fe86dda984251f87
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-01 06:14:35 +0700

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -134,6 +134,10 @@ def is_url(url: str):
     return False
 
 
+def is_mega_link(url: str):
+    return "mega.nz" in url
+
+
 def is_magnet(url: str):
     magnet = re.findall(MAGNET_REGEX, url)
     if magnet:

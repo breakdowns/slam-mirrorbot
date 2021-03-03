b02d6334b4b16a25907ca752feb913c971a62073
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-03 08:53:04 +0700

    Update list.py

--- bot/modules/list.py
+++ bot/modules/list.py
@@ -1,26 +1,27 @@
 from telegram.ext import CommandHandler, run_async
 from bot.helper.mirror_utils.upload_utils.gdriveTools import GoogleDriveHelper
 from bot import LOGGER, dispatcher
-from bot.helper.telegram_helper.message_utils import auto_delete_message, sendMessage
+from bot.helper.telegram_helper.message_utils import sendMessage, sendMarkup, editMessage
 from bot.helper.telegram_helper.filters import CustomFilters
 import threading
 from bot.helper.telegram_helper.bot_commands import BotCommands
 
 @run_async
 def list_drive(update,context):
-    if update.message.text == f'/{BotCommands.ListCommand}':
-        sendMessage(f'Send a search key along with {BotCommands.ListCommand} command', context.bot, update)
-    message = update.message.text
-    search = message.split(' ',maxsplit=1)[1]
-    LOGGER.info(f"Searching: {search}")
-    gdrive = GoogleDriveHelper(None)
-    msg = gdrive.drive_list(search)
-    if msg:
-        reply_message = sendMessage(msg, context.bot, update)
-    else:
-        reply_message = sendMessage('No result found', context.bot, update)
+    try:
+        search = update.message.text.split(' ',maxsplit=1)[1]
+        LOGGER.info(f"Searching: {search}")
+        reply = sendMessage('Searching..... Please wait!', context.bot, update)
+        gdrive = GoogleDriveHelper(None)
+        msg, button = gdrive.drive_list(search)
 
-    threading.Thread(target=auto_delete_message, args=(context.bot, update.message, reply_message)).start()
+        if button:
+            editMessage(msg, reply, button)
+        else:
+            editMessage('No result found', reply, button)
+
+    except IndexError:
+        sendMessage('Send a search key along with command', context.bot, update)
 
 
 list_handler = CommandHandler(BotCommands.ListCommand, list_drive,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)

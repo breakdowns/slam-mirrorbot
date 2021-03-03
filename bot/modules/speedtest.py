3520d590623be8f16bbabfb453e80b286e172858
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-03 11:56:30 +0700

    Update speedtest.py

--- bot/modules/speedtest.py
+++ bot/modules/speedtest.py
@@ -39,7 +39,7 @@ def speed_convert(size):
     """Hi human, you can't read bytes?"""
     power = 2 ** 10
     zero = 0
-    units = {0: "", 1: "Kb/s", 2: "Mb/s", 3: "Gb/s", 4: "Tb/s"}
+    units = {0: "", 1: "Kb/s", 2: "MB/s", 3: "Gb/s", 4: "Tb/s"}
     while size > power:
         size /= power
         zero += 1

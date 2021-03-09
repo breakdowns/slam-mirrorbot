014921d13c156b65750b774a2d19cd6bece6f9e7
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-10 03:59:30 +0700

    Update message_utils.py

--- bot/helper/telegram_helper/message_utils.py
+++ bot/helper/telegram_helper/message_utils.py
@@ -68,9 +68,9 @@ def delete_all_messages():
 
 def update_all_messages():
     msg = get_readable_message()
-    msg += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
-           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%" \
-           f" <b>RAM:</b> {psutil.virtual_memory().percent}%"
+    msg += f"<b>CPU:</b> {psutil.cpu_percent()}%\n" \
+           f"<b>RAM:</b> {psutil.virtual_memory().percent}%\n" \
+           f"<b>DISK:</b> {psutil.disk_usage('/').percent}%\n"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
@@ -104,9 +104,9 @@ def update_all_messages():
 
 def sendStatusMessage(msg, bot):
     progress = get_readable_message()
-    progress += f"<b>CPU:</b> {psutil.cpu_percent()}%" \
-           f" <b>DISK:</b> {psutil.disk_usage('/').percent}%" \
-           f" <b>RAM:</b> {psutil.virtual_memory().percent}%"
+    progress += f"<b>CPU:</b> {psutil.cpu_percent()}%\n" \
+           f"<b>RAM:</b> {psutil.virtual_memory().percent}%\n" \
+           f"<b>DISK:</b> {psutil.disk_usage('/').percent}%\n"
     with download_dict_lock:
         dlspeed_bytes = 0
         uldl_bytes = 0
@@ -124,7 +124,7 @@ def sendStatusMessage(msg, bot):
                     uldl_bytes += float(speedy.split('M')[0]) * 1048576
         dlspeed = get_readable_file_size(dlspeed_bytes)
         ulspeed = get_readable_file_size(uldl_bytes)
-        progress += f"\n<b>DL:</b>{dlspeed}ps 🔻| <b>UL:</b>{ulspeed}ps 🔺\n"
+        progress += f"\n<b>DL: </b>{dlspeed}ps 🔻| <b>UL: </b>{ulspeed}ps 🔺\n"
     with status_reply_dict_lock:
         if msg.message.chat.id in list(status_reply_dict.keys()):
             try:

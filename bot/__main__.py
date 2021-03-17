fed9c80ade350ac553a74db7eb52186198c4e18a
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-17 11:25:51 +0700

    Added repo link in /start command

--- bot/__main__.py
+++ bot/__main__.py
@@ -44,7 +44,7 @@ def stats(update, context):
 @run_async
 def start(update, context):
     start_string = f'''
-Hi, I'm Slam, a multipurpose bot for [hafitz](t.me/hafitzXD)
+Hi, I'm [Slam](https://github.com/breakdowns/slam-mirrorbot), a multipurpose bot for [hafitz](t.me/hafitzXD)
 Type /{BotCommands.HelpCommand} to get a list of available commands
 '''
     update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", start_string, parse_mode=ParseMode.MARKDOWN)

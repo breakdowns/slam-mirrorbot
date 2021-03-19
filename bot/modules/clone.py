227420d790d31f96ab8bb582f6b010daa0f4ee7f
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-19 09:27:46 +0700

    V4.3 (#30)
    - Fixed Zippyshare.
    Thanks to UsergeTeam, BianSepang, usmanmughalji 
    - Update cloning log

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -4,18 +4,32 @@ from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
-from bot import dispatcher
+from bot import dispatcher, LOGGER
 
 
 @new_thread
 def cloneNode(update,context):
+    if update.message.from_user.last_name:
+        last_name = f" {update.message.from_user.last_name}"
+    else:
+        last_name = ""
+    if update.message.from_user.username:
+        username = f"@{update.message.from_user.username}"
+    else:
+        username = ""
+    name = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}{last_name}</a>'
+
     args = update.message.text.split(" ",maxsplit=1)
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
+        msg = f'Cloning...\n' \
+              f'User: {username}\n' \
+              f'Link: <a href="{link}">{link}</a>'
+        sendMessage(msg, context.bot, update)
         gd = GoogleDriveHelper()
         result, button = gd.clone(link)
-        deleteMessage(context.bot,msg)
+        LOGGER.info('ID: {} - Username: {} - Message: {}'.format(update.message.chat.id,update.message.chat.username,update.message.text))
+        # deleteMessage(context.bot,msg)
         sendMarkup(result,context.bot,update,button)
     else:
         sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)

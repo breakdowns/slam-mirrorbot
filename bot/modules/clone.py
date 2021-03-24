1a222a3cfc5fe5825d4bff961cf657efa36e8c87
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-03-24 07:24:16 +0700

    v4.4.4 (#35)
    - cc after clone finish.
    Thanks to anasty17
    - Change to yt-dlp

--- bot/modules/clone.py
+++ bot/modules/clone.py
@@ -4,33 +4,28 @@ from bot.helper.telegram_helper.message_utils import *
 from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.ext_utils.bot_utils import new_thread
-from bot import dispatcher, LOGGER
+from bot import dispatcher
 
 
 @new_thread
 def cloneNode(update,context):
-    if update.message.from_user.last_name:
-        last_name = f" {update.message.from_user.last_name}"
-    else:
-        last_name = ""
-    if update.message.from_user.username:
-        username = f"@{update.message.from_user.username}"
-    else:
-        username = ""
-    name = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}{last_name}</a>'
-
     args = update.message.text.split(" ",maxsplit=1)
+    if update.message.from_user.username:
+        uname = f"@{update.message.from_user.username}"
+    else:
+        uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+    if uname is not None:
+            cc = f'\n\ncc: {uname}'
     if len(args) > 1:
         link = args[1]
-        msg = f'Cloning...\n' \
-              f'User: {username}\n' \
-              f'Link: <a href="{link}">{link}</a>'
-        sendMessage(msg, context.bot, update)
+        msg = sendMessage(f"Cloning: <code>{link}</code>",context.bot,update)
         gd = GoogleDriveHelper()
         result, button = gd.clone(link)
-        LOGGER.info('ID: {} - Username: {} - Message: {}'.format(update.message.chat.id,update.message.chat.username,update.message.text))
-        # deleteMessage(context.bot,msg)
-        sendMarkup(result,context.bot,update,button)
+        deleteMessage(context.bot,msg)
+        if button == "":
+            sendMessage(result,context.bot,update)
+        else:
+            sendMarkup(result + cc,context.bot,update,button)
     else:
         sendMessage("Provide G-Drive Shareable Link to Clone.",context.bot,update)
 

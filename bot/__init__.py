a5f6cec91b95ce172634f93cf93e4698b2e59fa1
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-04-12 09:57:14 +0700

    Update __init__.py

--- bot/__init__.py
+++ bot/__init__.py
@@ -91,6 +91,20 @@ except KeyError as e:
     exit(1)
 
 try:
+    MAX_TORRENT_SIZE = getConfig('MAX_TORRENT_SIZE')
+except KeyError:
+    MAX_TORRENT_SIZE = None
+
+try:
+   ENABLE_FILESIZE_LIMIT = getConfig('ENABLE_FILESIZE_LIMIT')
+   if ENABLE_FILESIZE_LIMIT.lower() == 'true':
+       ENABLE_FILESIZE_LIMIT = True
+   else:
+       ENABLE_FILESIZE_LIMIT = False
+except KeyError:
+    ENABLE_FILESIZE_LIMIT = False
+
+try:
     if os.environ['USE_TELEGRAPH'].upper() == 'TRUE':
         USE_TELEGRAPH = True
     else:

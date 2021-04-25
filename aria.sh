981db603ea0b8f3b17f0b6d4eabd71d4b6f360cf
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-04-26 01:04:17 +0700

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -2,7 +2,7 @@ export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
 export MAX_CONCURRENT_DOWNLOADS=7
 
-aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false\
+aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
    --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --seed-time=0.01 --min-split-size=10M \
    --follow-torrent=mem --split=10 \

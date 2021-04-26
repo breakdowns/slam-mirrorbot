3511b28e88b87dd43f30ee781f266cee77e2a255
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-04-26 16:10:43 +0700

    Change tracker

--- aria.sh
+++ aria.sh
@@ -1,5 +1,5 @@
 export MAX_DOWNLOAD_SPEED=0
-tracker_list=$(curl -Ns https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_all.txt | awk '$1' | tr '\n' ',')
+tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',')
 export MAX_CONCURRENT_DOWNLOADS=7
 
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \

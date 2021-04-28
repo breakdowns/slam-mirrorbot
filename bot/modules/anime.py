ac0cefa8d4960ff10a7fc4a0766715df903a0b50
Amirul Andalib <70439168+AmirulAndalib@users.noreply.github.com>
2021-04-28 21:48:28 +0600

    Update anime.py
    /\:

--- bot/modules/anime.py
+++ bot/modules/anime.py
@@ -256,7 +256,7 @@ def weebhelp(update, context):
 • `/character`*:* search character
 • `/manga`*:* search manga
 '''
-    update.effective_message.reply_photo("https://telegra.ph/file/db03910496f06094f1f7a.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
+    update.effective_message.reply_photo("https://telegra.ph/file/da.jpg", help_string, parse_mode=ParseMode.MARKDOWN)
 
 
 ANIME_HANDLER = CommandHandler("anime", anime)

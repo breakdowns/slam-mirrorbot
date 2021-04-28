7686c2231d695e1cef052930abbf4719698971a8
Amirul Andalib <70439168+AmirulAndalib@users.noreply.github.com>
2021-04-28 21:52:19 +0600

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -67,7 +67,7 @@ def chat_list(update, context):
 def repo(update, context):
     bot.send_message(update.message.chat_id,
     reply_to_message_id=update.message.message_id,
-    text="R", disable_web_page_preview=True)
+    text="", disable_web_page_preview=True)
 
 
 @run_async

0102c99294efd3c03b2b6060b393530f47238955
Xazmah <xazmah@protonmail.com>
2021-05-04 13:24:03 +0000

    Update aria.sh

--- aria.sh
+++ aria.sh
@@ -1,13 +1,13 @@
 export MAX_DOWNLOAD_SPEED=0
 tracker_list=$(curl -Ns https://raw.githubusercontent.com/XIU2/TrackersListCollection/master/all.txt | awk '$1' | tr '\n' ',')
-export MAX_CONCURRENT_DOWNLOADS=7
+export MAX_CONCURRENT_DOWNLOADS=8
 
 aria2c --enable-rpc --rpc-listen-all=false --rpc-listen-port 6800 --check-certificate=false \
    --max-connection-per-server=10 --rpc-max-request-size=1024M \
    --bt-tracker="[$tracker_list]" --bt-max-peers=0 --bt-tracker-connect-timeout=300 --bt-stop-timeout=1200 --seed-time=0.01 --min-split-size=10M \
    --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=$MAX_DOWNLOAD_SPEED \
-   --max-overall-upload-limit=1K --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
+   --max-concurrent-downloads=$MAX_CONCURRENT_DOWNLOADS \
    --peer-id-prefix=-TR2940- --user-agent=Transmission/2.94 --peer-agent=Transmission/2.94 --continue=true \
    --disk-cache=64M --file-allocation=prealloc \
    --max-file-not-found=5 --max-tries=20 --auto-file-renaming=true \

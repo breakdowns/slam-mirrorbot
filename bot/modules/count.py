230c5b61cf136bd72fedd7edc0e1a8599f9c0b7b
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-06-02 19:25:53 +0700

    Update count.py

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -5,6 +5,7 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot import dispatcher
 
+
 @run_async
 def countNode(update,context):
     args = update.message.text.split(" ",maxsplit=1)

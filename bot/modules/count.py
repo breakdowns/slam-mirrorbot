29d9fe93da2fd6d925e127569f5820472355122d
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-06-02 19:40:34 +0700

    Add cc

--- bot/modules/count.py
+++ bot/modules/count.py
@@ -7,17 +7,23 @@ from bot import dispatcher
 
 
 @run_async
-def countNode(update,context):
-    args = update.message.text.split(" ",maxsplit=1)
+def countNode(update, context):
+    args = update.message.text.split(" ", maxsplit=1)
+    if update.message.from_user.username:
+        uname = f"@{update.message.from_user.username}"
+    else:
+        uname = f'<a href="tg://user?id={update.message.from_user.id}">{update.message.from_user.first_name}</a>'
+    if uname is not None:
+            cc = f'\n\ncc: {uname}'
     if len(args) > 1:
         link = args[1]
-        msg = sendMessage(f"Counting: <code>{link}</code>",context.bot,update)
+        msg = sendMessage(f"Counting: <code>{link}</code>", context.bot, update)
         gd = GoogleDriveHelper()
         result = gd.count(link)
         deleteMessage(context.bot,msg)
-        sendMessage(result,context.bot,update)
+        sendMessage(result + cc, context.bot,update)
     else:
-        sendMessage("Provide G-Drive Shareable Link to Count.",context.bot,update)
+        sendMessage("Provide G-Drive Shareable Link to Count.", context.bot, update)
 
-count_handler = CommandHandler(BotCommands.CountCommand,countNode,filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
+count_handler = CommandHandler(BotCommands.CountCommand, countNode, filters=CustomFilters.authorized_chat | CustomFilters.authorized_user)
 dispatcher.add_handler(count_handler)

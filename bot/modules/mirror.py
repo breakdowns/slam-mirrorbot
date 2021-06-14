cdbb30b0cc12a2064100b54560ff907c32bd8768
Hafitz Setya <71178188+breakdowns@users.noreply.github.com>
2021-06-15 01:23:34 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -287,7 +287,16 @@ def _mirror(bot, update, isTar=False, extract=False):
     try:
         link = direct_link_generator(link)
     except DirectDownloadLinkException as e:
-        LOGGER.info(f'{link}: {e}')
+        LOGGER.info(f'{e}')
+        if "ERROR:" in str(e):
+            sendMessage(f"{e}", bot, update)
+            return
+        if "G-Drive" in str(e):
+            sendMessage(f"ERROR: {e}", bot, update)
+            return
+        if "Youtube" in str(e):
+            sendMessage(f"ERROR: {e}", bot, update)
+            return
     listener = MirrorListener(bot, update, pswd, isTar, tag, extract)
     if bot_utils.is_mega_link(link):
         link_type = get_mega_link_type(link)

89382ce62f476c17be9d7628dd63c1c55f5b9fce
Hafitz Setya <hafitz666@outlook.co.id>
2021-06-20 08:36:04 +0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,7 +1,6 @@
 import shutil, psutil
 import signal
 import os
-import importlib
 
 from pyrogram import idle
 from bot import app
@@ -19,11 +18,7 @@ from bot.helper.telegram_helper.message_utils import *
 from .helper.ext_utils.bot_utils import get_readable_file_size, get_readable_time
 from .helper.telegram_helper.filters import CustomFilters
 from bot.helper.telegram_helper import button_build
-from bot.modules import ALL_MODULES # Auto Load all modules without name problems
-
-for module in ALL_MODULES:
-    imported_module = importlib.import_module("bot.modules." + module)
-    importlib.reload(imported_module)
+from .modules import authorize, list, cancel_mirror, mirror_status, mirror, clone, watch, shell, eval, search, delete, speedtest, usage, mediainfo, count, config, updates
 
 now=datetime.now(pytz.timezone('Asia/Jakarta'))
 

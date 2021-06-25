7354e31e9262568ca1f8068cfce4c85aef44ad71
Katarina Novi <86172488+katarina-ox@users.noreply.github.com>
2021-06-25 08:43:43 +0700

    Add loading credentials externally

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
+if [[ -n $TOKEN_PICKLE_URL ]]; then
+	wget -q $TOKEN_PICKLE_URL -O /usr/src/app/token.pickle
+fi
+
+if [[ -n $ACCOUNTS_ZIP_URL ]]; then
+	wget -q $ACCOUNTS_ZIP_URL -O /usr/src/app/accounts.zip
+	unzip accounts.zip -d /usr/src/app/accounts
+	rm accounts.zip
+fi
+
 ./aria.sh; python3 -m bot

63b476d3bc1ae8e63c74c618481102556ced6d3f
Hafitz Setya <hafitz666@outlook.co.id>
2021-07-20 19:24:50 +0700

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -226,7 +226,7 @@ def github(url: str) -> str:
 
 
 def hxfile(url: str) -> str:
-    """ Racaty direct link generator
+    """ Hxfile direct link generator
     Based on https://github.com/breakdowns/slam-mirrorbot """
     bypasser = lk21.Bypass()
     dl_url=bypasser.bypass_filesIm(url)

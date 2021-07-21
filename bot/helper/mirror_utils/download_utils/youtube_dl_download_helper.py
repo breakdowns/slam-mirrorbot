dd814ab9b1b348a2804c6d52f56df3cae4dad0d5
FireHound-Lab <Machintosh117@protonmail.com>
2021-07-21 09:22:02 +0530

    Added support for Indian streaming sites
    Like Hotstar, Zee5, Mx player online etc.

--- bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
+++ bot/helper/mirror_utils/download_utils/youtube_dl_download_helper.py
@@ -1,6 +1,6 @@
 from .download_helper import DownloadHelper
 import time
-from youtube_dl import YoutubeDL, DownloadError
+from yt_dlp import YoutubeDL, DownloadError
 from bot import download_dict_lock, download_dict
 from ..status_utils.youtube_dl_download_status import YoutubeDLDownloadStatus
 import logging

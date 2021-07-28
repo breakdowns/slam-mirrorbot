ec604b0cb300296e3cb8e6488d9a552289b0048a
Hafitz Setya <hafitz666@outlook.co.id>
2021-07-28 10:49:07 +0700

    Change

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -557,9 +557,9 @@ class GoogleDriveHelper:
                     content += f'<b> | <a href="https://telegra.ph/{self.path[nxt_page]}">Next</a></b>'
                     nxt_page += 1
             Telegraph(access_token=telegraph_token).edit_page(path = self.path[prev_page],
-                                 title = 'Slam Mirror Bot Search',
-                                 author_name='Slam Mirror Bot',
-                                 author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                 title = 'Slam Aria Mirror Bot Search',
+                                 author_name='Slam Aria Mirror Bot',
+                                 author_url='https://github.com/breakdowns/slam-aria-mirror-bot',
                                  html_content=content)
         return
 
@@ -642,9 +642,9 @@ class GoogleDriveHelper:
 
             for content in self.telegraph_content :
                 self.path.append(Telegraph(access_token=telegraph_token).create_page(
-                                                        title = 'Slam Mirror Bot Search',
-                                                        author_name='Slam Mirror Bot',
-                                                        author_url='https://github.com/breakdowns/slam-mirrorbot',
+                                                        title = 'Slam Aria Mirror Bot Search',
+                                                        author_name='Slam Aria Mirror Bot',
+                                                        author_url='https://github.com/breakdowns/slam-aria-mirror-bot',
                                                         html_content=content
                                                         )['path'])
 

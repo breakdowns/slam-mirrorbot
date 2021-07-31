e56ad9522a74ff376bbf4245f764bd21a3463648
hyPnOtICDo0g <44530973+hyPnOtICDo0g@users.noreply.github.com>
2021-07-31 14:43:21 +0530

    remove shell perms for auth user & add sudo

--- bot/modules/shell.py
+++ bot/modules/shell.py
@@ -39,5 +39,5 @@ def shell(update, context):
 
 
 SHELL_HANDLER = CommandHandler(BotCommands.ShellCommand, shell, 
-                                                  filters=CustomFilters.owner_filter | CustomFilters.authorized_user, run_async=True)
+                                                  filters=CustomFilters.owner_filter | CustomFilters.sudo_user, run_async=True)
 dispatcher.add_handler(SHELL_HANDLER)

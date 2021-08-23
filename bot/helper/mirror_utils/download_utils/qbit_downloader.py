dc8b9dee247f595bcb8661bb69bc3c9dccc60e28
Hafitz Setya <hafitz666@outlook.co.id>
2021-08-23 14:32:21 +0700

    Update qbit_downloader.py

--- bot/helper/mirror_utils/download_utils/qbit_downloader.py
+++ bot/helper/mirror_utils/download_utils/qbit_downloader.py
@@ -1,5 +1,5 @@
-# Implement By - @anasty17 (https://github.com/Slam-Team/slam-mirrorbot/commit/0bfba523f095ab1dccad431d72561e0e002e7a59)
-# (c) https://github.com/Slam-Team/slam-mirrorbot
+# Implement By - @anasty17 (https://github.com/SlamDevs/slam-mirrorbot/commit/0bfba523f095ab1dccad431d72561e0e002e7a59)
+# (c) https://github.com/SlamDevs/slam-mirrorbot
 # All rights reserved
 
 import os

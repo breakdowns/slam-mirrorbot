b63a836872998b8c6f8310dd54bc8880edcb1bd9
Hafitz Setya <hafitz666@outlook.co.id>
2021-09-25 17:23:19 +0700

    Update qbit

--- aria.sh
+++ aria.sh
@@ -4,7 +4,7 @@ aria2c --enable-rpc --check-certificate=false \
    --bt-stop-timeout=0 --min-split-size=10M --follow-torrent=mem --split=10 \
    --daemon=true --allow-overwrite=true --max-overall-download-limit=0 --bt-tracker="[$tracker_list]"\
    --max-overall-upload-limit=1K --max-concurrent-downloads=15 --continue=true \
-   --peer-id-prefix=-qB4360- --user-agent=qBittorrent/4.3.6 --peer-agent=qBittorrent/4.3.6 \
+   --peer-id-prefix=-qB4380- --user-agent=qBittorrent/4.3.8 --peer-agent=qBittorrent/4.3.8 \
    --disk-cache=32M --bt-enable-lpd=true --seed-time=0 --max-file-not-found=0 \
    --max-tries=20 --auto-file-renaming=true --reuse-uri=true --http-accept-gzip=true \
    --content-disposition-default-utf8=true --netrc-path=/usr/src/app/.netrc
